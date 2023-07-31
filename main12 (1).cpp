eb540700e6366de2bde7244bf752fe4b46336ab4
Urvija jha <121400459+dosasambhar@users.noreply.github.com>
2023-08-01 02:06:47 +0530

    Q2- print column sum

---
+++ main12 (1).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ void funsum (int arr[][3], int rows, int colums){
+   for(int j=0; j<3; j++){
+     int sum =0;
+     for(int i=0; i<3; i++){
+       sum = sum+ arr[i][j];
+         }
+     cout<<sum<<endl;
+   }
+
+ }
+
+int main(){
+   int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+
+  funsum (arr,3,3);
+  return 0;
+}
