3879c70f222aac8a1c476472c5e18fc8c894bfe1
Urvija jha <121400459+dosasambhar@users.noreply.github.com>
2023-08-02 08:01:58 +0530

    Q3 linear search

--- main12.cpp
+++ main12.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
- void funsum (int arr[][3], int rows, int colums){
-   for(int i=0; i<3; i++){
-     int sum =0;
-     for(int j=0; j<3; j++){
-       sum = sum+ arr[i][j];
-         }
-     cout<<sum<<endl;
-   }
 
- }
+int main() {
+ int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+  cout<<"enter";
+  int key;
+  cin>>key;
+  bool found = false;
 
-int main(){
-   int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-
-  funsum (arr,3,3);
-  return 0;
-}
+  for(int i =0; i<3; i++){
+    for(int j=0; j<3; j++){
+      if( a[i][j]==key){
+        found = true; 
+        break;
+      }
+       }
+    if(found)
+       break;
+    }
+  if(found){
+    cout<<"found";
+  }
+  else{
+    cout<<"not";
+  }
+  }
+  
